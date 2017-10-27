f6cad4dd3cf6717d87451ff89e12470defefd0cf
liulian0519 <1223342263@qq.com>
2017-10-27 20:45:22 +0800

    封装增删改查模块

--- 5/02.js
+++ 5/02.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 var db = require("./model/db.js");
-app.get("/",function(req,res){
+增
+app.get("/charu",function(req,res){
    db.insertOne("student",{"name":"liulian"},function(err,result){
        if(err)
        {
@@ -11,6 +12,7 @@ app.get("/",function(req,res){
        res.send("插入成功");
    });
 });
+//查
 app.get("/du",function(req,res){
     var page = parseInt(req.query.page);
     console.log(page);
@@ -20,6 +22,26 @@ app.get("/du",function(req,res){
         }
 
         res.send(result)
+    });
+});
+//删除
+app.get("/shan",function(req,res){
+    var student_id=req.query.id;
+    db.deleteMany("student",{"student_id":student_id},function(err,result){
+        res.send(result);
+    });
+});
+//修改
+app.get("/xiugai",function(req,res){
+    // var student_id=req.query.id;
+    db.updateMany("student",{"student_id":"123456"},{
+        $set:{"name":"liulianlian"},
+    },function(err,result){
+        if(err){
+            console.log(err);
+            db.close();
+        }
+        res.send(result);
     });
 });
 app.listen(3000);
--- 5/model/db.js
+++ 5/model/db.js
@@ -1,11 +1,18 @@
 //这个模块封装了所有对数据库的常用操作
 //连接数据库
 var mongodb = require('mongodb');
+var settings = require("../setting");
+
 var MongoClient = require('mongodb').MongoClient;
 function _connectDB(callback){
-    var url = 'mongodb://localhost:27017/haha';
+    var url =settings.dburl; //从文件中读数据库地址
     MongoClient.connect(url,function(err,db){
+        if(err){
+            callback(err,null);
+            return;
+        }
         callback(err,db);
+        db.close();
     });
 }
 //插入数据
@@ -33,6 +40,7 @@ exports.find=function(collectionName,json,C,D){
         var args = C;
         var skipnumber = args.pageamount * args.page;
         var limit=args.pageamount;
+
         console.log(skipnumber);
         console.log(limit);
     }else{
@@ -43,15 +51,42 @@ exports.find=function(collectionName,json,C,D){
         var cursor = db.collection(collectionName).find(json).skip(skipnumber).limit(limit);
         cursor.each(function(err,doc){
             if(err){
-                return;
                 callback(err,null);
+
+                db.close();
+                return;
             }
             if(doc!=null){
                 result.push(doc);   //放入结果数组
             }else{
                 //遍历结束，没有更多文档了
                 callback(null,result);
+                db.close();
             }
         });
     });
+}
+//删除
+exports.deleteMany=function(collectionNmae,json,callback){
+    _connectDB(function(err,db){
+        db.collection(collectionNmae).deleteMany(
+            json,
+            function(err,results){
+                callback(err,results);
+                db.close();
+            }
+        );
+    });
+}
+//修改
+exports.updateMany=function(collectionNmae,json1,json2,callback){
+    _connectDB(function(err,db){
+        db.collection(collectionNmae).updateMany(
+            json1,
+            json2,
+            function(err,results){
+                callback(err,results);
+        });
+
+    })
 }
