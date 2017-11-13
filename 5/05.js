36257abe264f227f1c8cef6e9fce90d810121d6a
liulian0519 <1223342263@qq.com>
2017-11-13 22:12:30 +0800

    :ok_hand: improtant login and regist

--- 5/05.js
+++ 5/05.js
@@ -41,7 +41,26 @@ app.get("/doregist",function(req,res,next){
 app.post("/dologin",function(req,res,next){
     var form = new formidable.IncomingForm();
     form.parse(req,function(err,fields,files){
-        res.send(fields.dengluming);
+        // res.send(fields.dengluming);
+        var dengluming = fields.dengluming;
+        var mima = fields.mima;
+        mima = md5(md5(mima).substr(4,7) + md5(mima));
+        //检索数据库 按登录名查看密码是否正确
+        db.find("users",{"dengluming":dengluming},function(err,result){
+            if(result.length == 0){
+                res.send("-2"); //-2 表示没有这个人
+                return;
+            }
+            var shujukuzhongmima = result[0].mima;
+            // console.log(shujukuzhongmima);
+            //加密  与数据库中的密码进行比对
+            if(mima == shujukuzhongmima){
+                res.send("1");
+            }else{
+                res.send("-1");
+            }
+
+        })
     });
      return;
 });
